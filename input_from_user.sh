eee33e58af8336f84eac605eeada5f481c828c97
ashishamodkar <56507158+ashishamodkar@users.noreply.github.com>
2021-02-06 16:02:16 +0530

    Input and Multiple Input From user

---
+++ input_from_user.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# to take input from user use read command
+
+echo "Enter The name : "
+read name4 
+echo "Entered Name is $name4"
+
+# for  multiple input we can take multiple variable
+
+echo  "Enter your Full name : "
+read name1 name2 name3
+echo "Entred Name is $name1 $name2 $name3 "
